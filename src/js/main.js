f75413161bd8e7c56a51c0554e7b851329ef7004
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2022-01-17 20:29:37 +0900

    Delete src directory

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-const toogleBar = document.querySelector('.navbar .navbar_toogleButton');
-const menu = document.querySelector('.navbar .navbar_menu');
-const icons = document.querySelector('.navbar .navbar_icons');
-
-toogleBar.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
